a50ef4d0ea1ad462a80030bc42fdbae1cf775a21
DiogoPedrosaa <diogopedrosa07@gmail.com>
2025-02-12 20:43:07 -0300

    add protection filter for some routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,54 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HelloWorld from '../components/HelloWorld.vue'  
-import UserLogin from '../views/UserLogin.vue'
-import HomePage from '../views/HomePage.vue'
-import ListEmployees from '../views/ListEmployees.vue'
-import EditEmployee from '../views/EditEmployee.vue'
-import RegisterUser from '@/views/RegisterUser.vue'
-import AddEmployee from '@/views/addEmployee.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import HelloWorld from '../components/HelloWorld.vue';
+import UserLogin from '../views/UserLogin.vue';
+import HomePage from '../views/HomePage.vue';
+import ListEmployees from '../views/ListEmployees.vue';
+import EditEmployee from '../views/EditEmployee.vue';
+import RegisterUser from '@/views/RegisterUser.vue';
+import addEmployee from '@/views/addEmployee.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'home', component: HomePage },
   { path: '/login', name: 'login', component: UserLogin },
   { path: '/servidores', name: 'servidores', component: ListEmployees },
-  { path: '/servidores/edit/:id', name: 'edit-employee', component: EditEmployee },
-  { path: '/registrar', name: 'registrar', component: RegisterUser },
-  { path: '/adicionar', name:'adicionar', component: AddEmployee}
-]
+  { 
+    path: '/servidores/edit/:id', 
+    name: 'edit-employee', 
+    component: EditEmployee, 
+    meta: { requiresAuth: true }
+  },
+  { 
+    path: '/registrar', 
+    name: 'registrar', 
+    component: RegisterUser 
+  },
+  { 
+    path: '/adicionar-servidor', 
+    name: 'adicionar', 
+    component: addEmployee, 
+    meta: { requiresAuth: true } 
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(), 
   routes
-})
+});
 
-export default router
+
+function isAuthenticated() {
+  return !!localStorage.getItem('authToken'); 
+}
+
+
+router.beforeEach((to, from, next) => {
+
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    next({ name: 'login' }); 
+  } else {
+    next(); 
+  }
+});
+
+export default router;
+
